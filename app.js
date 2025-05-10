e6de2270f0246def9290d7fce61c5e445582f5ce
Juanclbici <juan.lopez4572@alumnos.udg.mx>
2025-05-09 18:24:51 -0600

    Sintaxis de desestructuración y require

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const saludos = require('./saludos.js');
+//sintaxis
+//const saludos = require('./saludos.js');
+//sintaxis de desestructuración
+const { saludar, saludarHolaMundo } = require('./saludos.js');
 
 //console.log(saludo);
 
-console.log(saludos.saludar('freeCodeCamp'));
-console.log(saludos.saludarHolaMundo());
+
+
+//Se usan en la primera sintaxis 
+//console.log(saludos.saludar('freeCodeCamp'));
+//console.log(saludo.saludarHolaMundo());
+
+//Sintaxis de desestructuración
+console.log(saludarHolaMundo());
+console.log(saludar( 'freeCodeCamp'));
