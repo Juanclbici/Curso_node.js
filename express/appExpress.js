f3fc13e9c7463c2e17a8de8de321b6da1032b7ad
Juanclbici <juan.lopez4572@alumnos.udg.mx>
2025-05-13 14:35:58 -0600

    Agregar rutas en Express

--- express/appExpress.js
+++ express/appExpress.js
@@ -9,6 +9,18 @@ app.get('/', (req, res) => {
     res.send('Mi primer servidor con Express. Cursos.');
 });
 
+app.get('/api/cursos', (req, res) => {
+    res.send(JSON.stringify(infoCursos));
+});
+
+app.get('/api/cursos/programacion', (req, res) => {
+    res.send(JSON.stringify(infoCursos.programacion));
+});
+
+app.get('/api/cursos/matematicas', (req, res) => {
+    res.send(JSON.stringify(infoCursos.matematicas));
+})
+
 const PUERTO = 3000;
 
 app.listen(PUERTO, () => {
